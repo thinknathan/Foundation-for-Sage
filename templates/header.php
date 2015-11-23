971bfc86e02b3adf44c03ad196873a3133bad83d
fredericpfisterer <fredericpfisterer@gmail.com>
2015-11-23 17:06:50 +0000

    fix foundation js and fix some css

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,14 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+<header>
+  <div class="top-bar">
+    <div class="row">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li class="menu-text"><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></li>
+          <?php if (has_nav_menu('primary_navigation')) :?>
+            <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => '', 'items_wrap' => '%3$s',]);?>
+          <?php endif;?>
+        </ul>
+      </div>
+    </div>
   </div>
 </header>
