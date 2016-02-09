5380cce0770b6437dc2ce791fae621c49f190de1
fredericpfisterer <fredericpfisterer@gmail.com>
2016-02-09 21:45:18 +0700

    Added some responsiveness. Feels better.

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,9 @@
 <header>
-  <div class="top-bar">
+  <div class="title-bar" data-responsive-toggle="top-menu" data-hide-for="medium">
+    <button class="menu-icon" type="button" data-toggle></button>
+    <div class="title-bar-title">Menu</div>
+  </div>
+  <div class="top-bar" id="top-menu">
     <div class="top-bar-left">
         <ul class="menu">
           <li class="menu-text"><?php bloginfo('name'); ?></li>
