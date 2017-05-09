e8910f4b380942d9e3014bf8cfcc43c7332c891a
Nathan Bolton <nathan@nathanbolton.ca>
2017-05-08 23:13:18 -0700

    Removing sidebar - more

--- lib/extras.php
+++ lib/extras.php
@@ -16,9 +16,11 @@ function body_class($classes) {
   }
 
   // Add class if sidebar is active
+  /*
   if (Setup\display_sidebar()) {
     $classes[] = 'sidebar-primary';
   }
+  */
 
   return $classes;
 }
