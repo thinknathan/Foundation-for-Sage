10098bcaadcbb441a43373855542a755eb42a15a
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:10:02 -0700

    Experimental structure, will change

--- templates/mobile-bottom-bar.php
+++ templates/mobile-bottom-bar.php
@@ -1,2 +1,5 @@
-<nav class="title-bar mobile-bottom-bar" data-sticky data-sticky-on="small" data-stick-to="bottom" data-options="marginBottom:0;">
+<nav class="mobile-bottom-bar" data-sticky data-sticky-on="small" data-stick-to="bottom" data-options="marginBottom:0;">
+  <a class="hollow button" href="#">
+    <span aria-hidden="true">&times;</span><span class="show-for-sr">Back to top</span>
+  </a>
 </nav>
