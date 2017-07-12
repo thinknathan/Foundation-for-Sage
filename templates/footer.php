26be89503805dc7951a04a26c09b81b02b73cae6
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:03:19 -0700

    Adds quick access footer, inline footer script

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,11 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p class="copyright">
+      &copy; <?= date('Y'); ?> Nathan Bolton
+    </p>
   </div>
 </footer>
-<footer class="quick-access-footer hide-for-medium" data-sticky-container>
+<footer class="hide-for-medium" data-sticky-container>
   <?php get_template_part('templates/mobile-bottom-bar'); ?>
 </footer>
+<?php $fileToInline = get_template_directory() . '/dist/scripts/' . basename(Roots\Sage\Assets\asset_path('scripts/footer-inline.js')); if ( file_exists($fileToInline) && filesize($fileToInline) > 0 ): ?><script><?php readfile($fileToInline); ?></script><?php endif; ?>
