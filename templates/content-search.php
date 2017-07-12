06ca803e372acdf07bd5570ed8854b97594651b4
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:00:50 -0700

    Adds custom snippet, metadata

--- templates/content-search.php
+++ templates/content-search.php
@@ -1,9 +1,17 @@
 <article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php if (get_post_type() === 'post') { get_template_part('templates/entry-meta'); } ?>
+  <header class="entry-header">
+    <h2 class="entry-title">
+      <a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?>
+      </a>
+    </h2>
   </header>
   <div class="entry-summary">
-    <?php the_excerpt(); ?>
+    <?= Roots\Sage\Extras\Snip(get_the_content(), '20', 'words'); ?>
   </div>
+  <?php if (get_post_type() === 'post'): ?>
+    <footer class="entry-footer">
+      <?php get_template_part('templates/entry-meta'); ?>
+    </footer>
+  <?php endif; ?>
 </article>
