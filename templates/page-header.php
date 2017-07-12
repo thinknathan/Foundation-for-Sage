eb98fba2b1b9763bc6bd142cb06b768b5695771f
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:09:00 -0700

    No title on homepage

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,13 @@
 <?php use Roots\Sage\Titles; ?>
 
+<?php if (!is_front_page()): ?>
+
 <div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
+  <div class="page-header-inner">
+    <h1>
+      <?= Titles\title(); ?>
+    </h1>
+  </div>
 </div>
+
+<?php endif; ?>
