260fe0db51b373688e4c6562e57412b805408fa1
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:00:09 -0700

    Only adds pagination if the page is paginated

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,4 @@
 <?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php if ( Roots\Sage\Extras\is_paginated_post() ): ?>
+  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php endif; ?>
