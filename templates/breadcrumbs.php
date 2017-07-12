d8b45a8b77587044ad1e852b23cd6b3fa5068073
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 22:59:24 -0700

    New template parts

---
+++ templates/breadcrumbs.php
@@ -0,0 +1,8 @@
+<?php
+if ( function_exists('yoast_breadcrumb') ) {
+  yoast_breadcrumb(
+    '<nav aria-label="You are here:" role="navigation" class="breadcrumbs"><p>',
+    '</p></nav>'
+  );
+}
+?>
---
+++ templates/card-article.php
@@ -0,0 +1,34 @@
+<a href="<?php the_permalink() ?>">
+  <div class="related-post card-post" data-equalizer-watch>
+
+    <div class="card-post-img">
+      <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-post-img']); ?>
+    </div>
+
+    <div class="card-post-content card-section">
+      <div class="card-post-avatar">
+        <?php echo get_avatar( get_the_author_meta( 'ID' ) , 32 ); ?>
+      </div>
+      <p class="card-post-name">
+        <?= Roots\Sage\Extras\Snip(get_the_title(), '9', 'words'); ?>
+      </p>
+      <p class="card-post-status hide">
+        By <?php the_author() ?>
+      </p>
+      <p class="card-post-status hide">
+        Posted <time datetime="<?= get_the_date('c'); ?>"><?= get_the_date('F j, Y'); ?></time>
+      </p>
+      <p class="card-post-status hide">
+        Modified <time datetime="<?= get_the_modified_date('c'); ?>"><?= get_the_modified_date('F j, Y'); ?></time>
+      </p>
+      <p class="card-post-info">
+        <?= Roots\Sage\Extras\Snip(get_the_content(), '15', 'words'); ?>
+      </p>
+    </div>
+
+    <div class="card-post-actions">
+      <span class="card-post-button button">Read More</span>
+    </div>
+
+  </div>
+</a>
