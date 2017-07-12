d48f21670a218a53b42fdc57da5ee284f6bdb440
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:08:12 -0700

    Restructure off-canvas

--- templates/offcanvas.php
+++ templates/offcanvas.php
@@ -1,6 +1,16 @@
-<div class="off-canvas position-right" id="offCanvas" data-off-canvas>
-	<?php get_search_form(); ?>
-	<?php if (has_nav_menu('primary_navigation')) :?>
-		<?php Roots\Sage\Extras\off_canvas_nav(); ?>
-	<?php endif;?>
+<div class="off-canvas position-right" id="offCanvas" data-off-canvas data-transition="overlap">
+  <div class="masthead">
+    <button class="close-button" aria-label="Close menu" type="button" data-close>
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <p><strong>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a>
+    </strong></p>
+  </div>
+  <div class="off-canvas-inner">
+    <?php get_search_form(); ?>
+    <?php if (has_nav_menu('primary_navigation')): ?>
+      <?php Roots\Sage\Extras\off_canvas_nav(); ?>
+    <?php endif; ?>
+  </div>
 </div>
