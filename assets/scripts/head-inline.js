9dc9f51b3c3b1414616c6c93a102395e1b369f0b
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:54:48 -0700

    Use IF statement instead of return for nutil.js detect

--- assets/scripts/head-inline.js
+++ assets/scripts/head-inline.js
@@ -44,35 +44,32 @@
   }
 
   // Google Analytics additional setup
-  // Requires Google Analytics and User ID Utility
+  // Requires Google Analytics and User ID Utility (nutil.js)
   if (window.ga && window.nUtil) {
-    var ga = window.ga;
-    var nUtil = window.nUtil;
     var uaID;
-  } else {
-    return;
+
+    // If there's a query string in the url, use it.
+    // This allows you to force a userID by setting the query:
+    // http://example.com/?refID=UserIDGoesRightHere
+
+    if (nUtil.getquery('refID')) {
+      uaID = nUtil.getquery('refID');
+      nUtil.set('userGaID', uaID, 360);
+
+      // If you have a cookie set, use it
+    } else if (nUtil.check('userGaID')) {
+      uaID = nUtil.get('userGaID');
+
+      // If this is their first visit, generate user ID
+    } else {
+      uaID = nUtil.createuid();
+      nUtil.set('userGaID', uaID, 360);
+    }
+
+    // Sets up userID
+    ga('set', 'userId', uaID);
   }
 
-  // If there's a query string in the url, use it.
-  // This allows you to force a userID by setting the query:
-  // http://example.com/?refID=UserIDGoesRightHere
-
-  if (nUtil.getquery('refID')) {
-    uaID = nUtil.getquery('refID');
-    nUtil.set('userGaID', uaID, 360);
-
-    // If you have a cookie set, use it
-  } else if (nUtil.check('userGaID')) {
-    uaID = nUtil.get('userGaID');
-
-    // If this is their first visit, generate user ID
-  } else {
-    uaID = nUtil.createuid();
-    nUtil.set('userGaID', uaID, 360);
-  }
-
-  // Sets up userID
-  ga('set', 'userId', uaID);
   // Insert your tracking ID
   ga('create', 'UA-XXXXX-Y', 'auto');
   // Uncomment if using Remarketing, Demographics and Interest Reporting
