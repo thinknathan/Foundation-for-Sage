6a6d95793d0db0be8ff431155cb47f9d35be6403
Nathan Bolton <nathan@nathanbolton.ca>
2017-07-11 23:10:39 -0700

    Restructure header

--- templates/header.php
+++ templates/header.php
@@ -1,17 +1,47 @@
-<header data-sticky-container>
-  <div class="title-bar" data-sticky data-sticky-on="small" data-options="marginTop:0;">
-    <div class="title-bar-left">
-      <strong>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a>
-      </strong>
-    </div>
-    <div class="title-bar-right">
-      <nav class="show-for-large">
-        <?php if (has_nav_menu('primary_navigation')): ?>
-          <?php Roots\Sage\Extras\top_nav(); ?>
-        <?php endif; ?>
-      </nav>
-      <button class="menu-icon hide-for-large" type="button" data-open="offCanvas"></button>
+<header class="site-header">
+
+  <div data-sticky-container>
+    <div class="title-bar-small headroom" data-sticky data-sticky-on="small" data-options="marginTop:0;" data-headroom>
+      <div class="container">
+        <div class="title-bar-left">
+          <h1 class="site-title">
+            <a href="<?= esc_url( home_url( '/' ) ); ?>" rel="home">
+              <?php bloginfo('name'); ?>
+            </a>
+          </h1>
+          <p class="site-description">
+            <?= get_bloginfo('description') ?>
+          </p>
+        </div>
+        <div class="title-bar-right">
+          <button class="menu-icon" type="button" data-open="offCanvas"></button>
+        </div>
+      </div>
     </div>
   </div>
+
+  <div class="site-hero">
+    <div class="container">
+      <div class="column">
+        <h1 class="site-title">
+          <a href="<?= esc_url( home_url( '/' ) ); ?>" rel="home">
+            <?php bloginfo('name'); ?>
+          </a>
+        </h1>
+        <p class="site-description">
+          <?= get_bloginfo('description') ?>
+        </p>
+      </div>
+    </div>
+  </div>
+  <?php if (has_nav_menu('primary_navigation')): ?>
+  <div data-sticky-container>
+    <nav class="site-nav" data-sticky data-sticky-on="large" data-stick-to="top" data-margin-top="0" data-anchor="document">
+      <div class="container">
+        <?php Roots\Sage\Extras\top_nav(); ?>
+      </div>
+    </nav>
+  </div>
+  <?php endif; ?>
+
 </header>
