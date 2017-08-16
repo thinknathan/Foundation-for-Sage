94e261e5eaf9d903dc46bd40b76adc50b8c48b1d
Nathan Bolton <nathan@nathanbolton.ca>
2017-08-15 23:37:38 -0700

    Minor formatting & WIP code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,35 +10,47 @@
  * always reference jQuery with $, even when in .noConflict() mode.
  * ======================================================================== */
 
-(function($) {
+(function ($) {
 
   // Use this variable to set up the common and page specific functions. If you
   // rename this variable, you will also need to rename the namespace below.
   var Sage = {
     // All pages
     'common': {
-      init: function() {
+      init: function () {
         // JavaScript to be fired on all pages
 
         $(document).foundation(); // Foundation JavaScript
         
+        // Runs queued jQuery functions
+        // From: https://stackoverflow.com/questions/8298430/handling-code-which-relies-on-jquery-before-jquery-is-loaded
+        /*
+        $(function () {
+          $.each(window.jQueryQ || [], function (i, a) {
+            setTimeout(function () {
+              $.apply(this, a);
+            }, 0);
+          });
+        });
+        */
+
       },
-      finalize: function() {
+      finalize: function () {
         // JavaScript to be fired on all pages, after page specific JS is fired
       }
     },
     // Home page
     'home': {
-      init: function() {
+      init: function () {
         // JavaScript to be fired on the home page
       },
-      finalize: function() {
+      finalize: function () {
         // JavaScript to be fired on the home page, after the init JS
       }
     },
     // About us page, note the change from about-us to about_us.
     'about_us': {
-      init: function() {
+      init: function () {
         // JavaScript to be fired on the about us page
       }
     }
@@ -47,7 +59,7 @@
   // The routing fires all common scripts, followed by the page specific scripts.
   // Add additional events for more control over timing e.g. a finalize event
   var UTIL = {
-    fire: function(func, funcname, args) {
+    fire: function (func, funcname, args) {
       var fire;
       var namespace = Sage;
       funcname = (funcname === undefined) ? 'init' : funcname;
@@ -59,12 +71,12 @@
         namespace[func][funcname](args);
       }
     },
-    loadEvents: function() {
+    loadEvents: function () {
       // Fire common init JS
       UTIL.fire('common');
 
       // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
+      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function (i, classnm) {
         UTIL.fire(classnm);
         UTIL.fire(classnm, 'finalize');
       });
