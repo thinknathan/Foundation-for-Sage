fb90a3c220734f9680a07aef9489a66ca252bb96
Nathan Bolton <nathan@nathanbolton.ca>
2017-09-18 22:20:53 -0700

    Add tinyMCE config to sanitize pasted content

--- lib/extras.php
+++ lib/extras.php
@@ -330,3 +330,27 @@ function auto_id_headings( $content ) {
     return $content;
 }
 add_filter( 'the_content', __NAMESPACE__ . '\\auto_id_headings' );
+
+
+/**
+ * Force the visual editor to strip extra formatting when pasting
+ * Credit: https://jonathannicol.com/blog/2015/02/19/clean-pasted-text-in-wordpress/
+ */
+function configure_tinymce( $in ) {
+  $in['paste_preprocess'] = "function(plugin, args){
+    // Strip all HTML tags except those we have whitelisted
+    var whitelist = 'br,p,span,b,strong,i,em,a,h1,h2,h3,h4,h5,h6,ul,li,ol';
+    var stripped = jQuery('<div>' + args.content + '</div>');
+    var els = stripped.find('*').not(whitelist);
+    for (var i = els.length - 1; i >= 0; i--) {
+      var e = els[i];
+      jQuery(e).replaceWith(e.innerHTML);
+    }
+    // Strip all class and id attributes
+    stripped.find('*').removeAttr('id').removeAttr('class').removeAttr('style');
+    // Return the clean HTML
+    args.content = stripped.html();
+  }";
+  return $in;
+}
+add_filter('tiny_mce_before_init', __NAMESPACE__ . '\\configure_tinymce');
