1f2b63a0d1ebb15605edcdb2bffcf84d27125532
Nathan Bolton <nathan@nathanbolton.ca>
2017-10-12 23:27:40 -0700

    Remove register sidebars

--- lib/setup.php
+++ lib/setup.php
@@ -103,30 +103,6 @@ function off_canvas_nav() {
 
 
 /**
- * Register sidebars
- */
-function widgets_init() {
-  register_sidebar([
-    'name'          => __('Primary', 'sage'),
-    'id'            => 'sidebar-primary',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
-
-  register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
-}
-//add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
-
-/**
  * Determine which pages should NOT display the sidebar
  */
 function display_sidebar() {
