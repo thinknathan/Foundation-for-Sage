5a45d2313307f7f2d39af9f7873641dd18762e41
Nathan Bolton <nathan@nathanbolton.ca>
2017-10-12 23:13:08 -0700

    Removed post formats

--- lib/setup.php
+++ lib/setup.php
@@ -49,10 +49,6 @@ function setup() {
   add_image_size( 'medium', 640, 640, false );
   add_image_size( 'xlarge', 1200, 820, false );
 
-  // Enable post formats
-  // http://codex.wordpress.org/Post_Formats
-  //add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
-
   // Enable HTML5 markup support
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
