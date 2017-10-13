65bcae836ac7fad406ae96a7ec9b2b5816eb7ebb
Nathan Bolton <nathan@nathanbolton.ca>
2017-10-12 23:19:23 -0700

    Added options to options page call

--- lib/setup.php
+++ lib/setup.php
@@ -59,7 +59,10 @@ function setup() {
   
   // ACF Options Page
   if( function_exists('acf_add_options_page') ) {
-    acf_add_options_page();
+    acf_add_options_page([
+      'page_title' => 'Options',
+      'icon_url' => 'dashicons-admin-generic'
+    ]);
   }
   
   // Support Yoast SEO Breadcrumbs
