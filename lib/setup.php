a3e28c199e8a700786c1753b70d84549e365d91a
Nathan Bolton <nathan@nathanbolton.ca>
2017-10-12 23:31:12 -0700

    Changed order of code to put menu code together

--- lib/setup.php
+++ lib/setup.php
@@ -43,13 +43,6 @@ function setup() {
   // Enable plugins to manage the document title
   // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
   add_theme_support('title-tag');
-
-  // Register wp_nav_menu() menus
-  // http://codex.wordpress.org/Function_Reference/register_nav_menus
-  register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage'),
-    'mobile_navigation' => __('Mobile Navigation', 'sage')
-  ]);
   
   // Enable post thumbnails
   // http://codex.wordpress.org/Post_Thumbnails
@@ -67,6 +60,9 @@ function setup() {
   // Enable HTML5 markup support
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+  
+  // Support Yoast SEO Breadcrumbs
+  add_theme_support('yoast-seo-breadcrumbs');
 
   // Use main stylesheet for visual editor
   // To add custom styles edit /assets/styles/layouts/_tinymce.scss
@@ -80,8 +76,13 @@ function setup() {
     ]);
   }
   
-  // Support Yoast SEO Breadcrumbs
-  add_theme_support('yoast-seo-breadcrumbs');
+  // Register wp_nav_menu() menus
+  // http://codex.wordpress.org/Function_Reference/register_nav_menus
+  register_nav_menus([
+    'primary_navigation' => __('Primary Navigation', 'sage'),
+    'mobile_navigation' => __('Mobile Navigation', 'sage')
+  ]);
+  
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
 
