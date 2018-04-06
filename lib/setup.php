8e20ac954f5d355aea97cc0eaee5221aca55c1ce
Nathan Bolton <nathan@nathanbolton.ca>
2018-04-05 21:53:42 -0700

    Optional: Set login redirects by user role

--- lib/setup.php
+++ lib/setup.php
@@ -121,6 +121,36 @@ add_action('acf/init', __NAMESPACE__ . '\\my_acf_init');
 
 
 /**
+ * Set login redirects by user role
+ */
+/*
+function login_redirect( $redirect_to, $request, $user ) {
+	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+		// Administrator role
+		if ( in_array( 'administrator', $user->roles ) ) {
+			return $redirect_to;
+    // Editor role
+		} elseif ( in_array( 'editor', $user->roles ) ) {
+			return $redirect_to;
+    // Author role
+    } elseif ( in_array( 'author', $user->roles ) ) {
+			return $redirect_to;
+    // Contributor role
+		} elseif ( in_array( 'contributor', $user->roles ) ) {
+			return $redirect_to;
+    // Subscriber role
+		} else {
+			return $redirect_to;
+		}
+	} else {
+		return $redirect_to;
+	}
+}
+add_filter( 'login_redirect', __NAMESPACE__ . '\\login_redirect', 20, 3 );
+*/
+
+
+/**
  * Top menu
  * Credit to chuckn246 + JointsWP Menu Code (https://github.com/JeremyEnglert/JointsWP)
  */
