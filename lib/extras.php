1da3a502d9b053d339712617266ef0b0c43037b3
Nathan Bolton <nathan@nathanbolton.ca>
2019-05-16 21:01:53 -0700

    Changes Gravity Forms submit input to button

--- lib/extras.php
+++ lib/extras.php
@@ -394,3 +394,16 @@ remove_all_actions('woocommerce_sidebar');
 
 // Removes breadcrumbs
 remove_action( 'woocommerce_before_main_content','woocommerce_breadcrumb', 20, 0);
+
+
+/*
+ * Changes Gravity Forms submit input to button
+ * https://gist.github.com/mannieschumpert/8334811
+ */
+function form_submit_button ( $button, $form ){
+  $button = str_replace( "input", "button", $button );
+  $button = str_replace( "/", "", $button );
+  $button .= "{$form['button']['text']}</button>";
+  return $button;
+}
+add_filter( 'gform_submit_button', __NAMESPACE__ . '\\form_submit_button', 10, 5 );
