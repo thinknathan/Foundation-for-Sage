ad43ab5eddeac706490d99550faf6888e17f2b90
Nathan Bolton <nathan@nathanbolton.ca>
2019-05-18 10:47:54 -0700

    Update dependencies, lazysizes

--- assets/scripts/app.priority.js
+++ assets/scripts/app.priority.js
@@ -1,3 +1,4 @@
 import '../../node_modules/lazysizes/lazysizes.js';
 import '../../node_modules/lazysizes/plugins/respimg/ls.respimg.js';
 import '../../node_modules/lazysizes/plugins/bgset/ls.bgset.js';
+import '../../node_modules/lazysizes/plugins/native-loading/ls.native-loading.js';
