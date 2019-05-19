33fabcce9fa8e66295f21069038b15935815b754
Nathan Bolton <nathan@nathanbolton.ca>
2019-05-19 15:25:51 -0700

    Gforms adds jQ again

--- lib/gravity-forms.php
+++ lib/gravity-forms.php
@@ -13,3 +13,13 @@ function form_submit_button ( $button, $form ){
   return $button;
 }
 add_filter( 'gform_submit_button', __NAMESPACE__ . '\\form_submit_button', 10, 5 );
+
+
+/*
+ * Enqueues jQuery when Gravity Forms in on the page
+ * Not sure how stable this is, seems to make ajax forms buggy
+ */
+function add_jquery() {
+  wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-1.12.4.min.js', array(), null, false );
+}
+add_action( 'gform_enqueue_scripts',  __NAMESPACE__ . '\\add_jquery', 10 );
