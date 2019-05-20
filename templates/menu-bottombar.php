c54f69a9799666f682cc9af37cfaadda6cb67442
Nathan Bolton <nathan@nathanbolton.ca>
2019-05-20 01:30:19 -0700

    return menu-bottombar to production

---
+++ templates/menu-bottombar.php
@@ -0,0 +1,11 @@
+<nav class="button-group expanded stick-to-bottom menu-bottombar" aria-label="Frequently used links">
+  <a class="button" href="#">
+    Button 1
+  </a>
+  <a class="button" href="#">
+    Button 2
+  </a>
+  <a class="button" href="#">
+    Button 3
+  </a>
+</nav>
