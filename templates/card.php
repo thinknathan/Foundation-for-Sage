878decdda5d866870e1c54511b2ee36b51c3a7e8
Nathan Bolton <nathan@nathanbolton.ca>
2019-05-20 01:28:56 -0700

    Improved accessibility of cards

--- templates/card.php
+++ templates/card.php
@@ -1,24 +1,44 @@
-<?php use Roots\Sage\Util ?>
+<?php
+/*
+ * Foundation Card element
+ * Add `.card-image-first` to swap order of photo and title section
+ * Remove `.card-single-link` to make only the anchor part of the card clickable
 
-<a href="<?php the_permalink() ?>" class="card">
-  <div class="card-section">
+ ===================================
+
+ More elements you may want to add:
+
+  <div class="card-avatar">
+    <?php echo get_avatar( get_the_author_meta( 'ID' ) , 32 ); ?>
+  </div>
+  <p class="card-meta">
+    Posted <time datetime="<?= get_the_date('c'); ?>"><?= get_the_date('F j, Y'); ?></time>
+  </p>
+  <p class="card-meta">
+    Modified <time datetime="<?= get_the_modified_date('c'); ?>"><?= get_the_modified_date('F j, Y'); ?></time>
+  </p>
+
+ ===================================
+
+ */
+
+ use Roots\Sage\Util;
+
+ // Encode the title as a base36 number for use as a unique ID
+ $card_id = intval(get_the_title(), 36);
+
+?>
+
+<li class="card card-<?= esc_attr( get_post_type() ) ?> card-single-link">
+  <div class="card-section card-section-head">
+
     <h2 class="h3">
-      <?= Util\snip(get_the_title(), '9', 'words'); ?>
+      <a href="<?php the_permalink() ?>" aria-describedby="card-button-<?= $card_id ?>"><?= Util\snip(get_the_title(), '9', 'words'); ?></a>
     </h2>
+
     <p class="card-meta">
       By <?php the_author() ?>
     </p>
-    <?php /*
-    <div class="card-avatar">
-      <?php echo get_avatar( get_the_author_meta( 'ID' ) , 32 ); ?>
-    </div>
-    <p class="card-meta">
-      Posted <time datetime="<?= get_the_date('c'); ?>"><?= get_the_date('F j, Y'); ?></time>
-    </p>
-    <p class="card-meta">
-      Modified <time datetime="<?= get_the_modified_date('c'); ?>"><?= get_the_modified_date('F j, Y'); ?></time>
-    </p>
-    */ ?>
   </div>
 
   <div class="card-image">
@@ -33,7 +53,7 @@
 
   <div class="card-section">
     <div class="button-group">
-      <span class="button">Read More</span>
+      <span class="button" aria-hidden="true" id="card-button-<?= $card_id ?>">Read More</span>
     </div>
   </div>
-</a>
+</li>
