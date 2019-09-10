125c20a5337282e0c283a7a300a0222a0b09e5bc
Nathan Bolton <nathan@nathanbolton.ca>
2019-09-10 15:42:37 -0700

    add js util for lazyloading video

---
+++ assets/scripts/util/lazyloadVideo.js
@@ -0,0 +1,18 @@
+import isVisible from './isVisible';
+
+/*
+ * Loads video sources that have their URI in data-src
+ * Moves data-src into src
+ */
+export default function (elem) {
+  if (!elem) return;
+
+  if (isVisible(elem)) {
+    var children = elem.getElementsByTagName('source');
+    for (let i = 0; i < children.length; ++i) {
+      children[i].src = children[i].getAttribute('data-src');
+    }
+    // Activates loading only if the video is not already loading or ready
+    if (elem.readyState < 1) elem.load();
+  }
+}
