390571f597b3017e42e5196b7db0fa2047a2f41b
Nathan Bolton <nathan@nathanbolton.ca>
2019-09-09 19:42:44 -0700

    remove the_excerpt filter

--- lib/extras.php
+++ lib/extras.php
@@ -123,15 +123,6 @@ add_filter('body_class', __NAMESPACE__ . '\\body_class');
 
 
 /**
- * Cleans up the_excerpt()
- */
-function excerpt_more() {
-  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
-}
-add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
-
-
-/**
  * Adds aria labels to the pagination
  */
 function prev_posts_link_attributes() {
