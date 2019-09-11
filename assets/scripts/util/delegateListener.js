32c20b8e3df643cee28586ffdfba8d1f1014f0f2
Nathan Bolton <nathan@nathanbolton.ca>
2019-09-11 13:51:50 -0700

    add delegate listener js util

---
+++ assets/scripts/util/delegateListener.js
@@ -0,0 +1,18 @@
+/*
+ * Add event listener to delegate.
+ *
+ * @param string   elem         Element(s) to select.
+ * @param string   delegate     Element(s) to delegate.
+ * @param string   eventType    Type of event to listen for.
+ * @param Function func         Function to run when element clicked.
+ */
+export default function(elem, delegate, eventType, fn) {
+  var delegateElem = document.querySelector(delegate);
+  if (delegateElem) {
+    delegateElem.addEventListener(eventType, function (event) {
+      if (event.target.matches(elem)) {
+        fn.call(event.target, event);
+      }
+    });
+  }
+}
