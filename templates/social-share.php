a86ad722a3d517b026be6be6e92e004b0974ea1e
Nathan Bolton <nathan@nathanbolton.ca>
2019-11-09 15:51:45 -0800

    minor tweaks

--- templates/social-share.php
+++ templates/social-share.php
@@ -12,19 +12,19 @@ $posturl = urlencode( esc_url( get_permalink() ));
 
 <div class="social-share">
 
-  <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $posturl; ?>&t=<?php echo urlencode( $post->post_title ); ?>" target="_blank" rel="noopener noreferrer" class="social-share__link button">
+  <a href="https://www.facebook.com/sharer/sharer.php?u=<?= $posturl; ?>&t=<?= urlencode( $post->post_title ); ?>" target="_blank" rel="noopener noreferrer" class="button social-share__link ">
     <?php esc_html_e( 'Share on Facebook', 'sage' ); ?><span class="show-for-sr"><?php esc_html_e( '(opens new window)', 'sage' ); ?></span>
   </a>
 
-  <a href="https://twitter.com/share?text=<?php the_title(); ?>&url=<?php echo $posturl; ?>" target="_blank" rel="noopener noreferrer" class="social-share__link button">
+  <a href="https://twitter.com/share?text=<?php the_title(); ?>&url=<?= $posturl; ?>" target="_blank" rel="noopener noreferrer" class="button social-share__link ">
     <?php esc_html_e( 'Share on Twitter', 'sage' ); ?>  <span class="show-for-sr"><?php esc_html_e( '(opens new window)', 'sage' ); ?></span>
   </a>
 
-  <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $posturl; ?>&title=<?php the_title(); ?>" target="_blank" rel="noopener noreferrer" class="social-share__link button">
+  <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?= $posturl; ?>&title=<?php the_title(); ?>" target="_blank" rel="noopener noreferrer" class="button social-share__link ">
     <?php esc_html_e( 'Share on LinkedIn', 'sage' ); ?> <span class="show-for-sr"><?php esc_html_e( '(opens new window)', 'sage' ); ?></span>
   </a>
 
-  <a href="http://www.pinterest.com/pin/create/bookmarklet/?url=<?php echo $posturl; ?><?php if(!empty($imgurl)): ?>&media=<?php echo $imgurl; endif; ?>&is_video=false&description=<?php echo urlencode( esc_attr( get_the_excerpt() ) ); ?>" target="_blank" rel="noopener noreferrer" class="social-share__link button">
+  <a href="http://www.pinterest.com/pin/create/bookmarklet/?url=<?= $posturl; ?><?php if(!empty($imgurl)): ?>&media=<?= $imgurl; endif; ?>&is_video=false&description=<?= urlencode( esc_attr( get_the_excerpt() ) ); ?>" target="_blank" rel="noopener noreferrer" class="button social-share__link ">
     <?php esc_html_e( 'Share on Pinterest', 'sage' ); ?> <span class="show-for-sr"><?php esc_html_e( '(opens new window)', 'sage' ); ?></span>
   </a>
 
