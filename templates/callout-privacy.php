f1801d04ea48745ea659400fc6407f4f7eb0c475
Nathan Bolton <nathan@nathanbolton.ca>
2019-11-12 22:34:51 -0800

    new privacy callout widget

---
+++ templates/callout-privacy.php
@@ -0,0 +1,22 @@
+<aside 
+       id="callout--privacy"
+       class="callout callout--primary callout--privacy callout--privacy--inactive"
+       aria-describedby="callout--privacy__content"
+       aria-labelledby="callout--privacy__title">
+  <!--googleoff: all-->
+  <div class="callout--privacy__inner">
+    
+    <h2 class="show-for-sr" id="callout--privacy__title">Privacy and Cookie Usage</h2>
+  
+    <p id="callout--privacy__content" class="callout--privacy__content">
+      We use cookies and other trackers to gauge the success of advertising, understand how visitors navigate our site, and improve the browsing experience.
+    </p>
+
+    <p class="button-group button-group--end">
+      <a href="#CHANGEME" id="callout--privacy__more" class="button button--clear button--clear--hollow">Learn More<span class="show-for-sr"> about privacy and cookies</span></a>
+      <button id="callout--privacy__close" class="button button--hollow button--primary--hollow">Accept <span class="show-for-sr"> privacy and cookie consent</span></button>
+    </p>
+  
+  </div>
+  <!--googleon: all-->
+</aside>
