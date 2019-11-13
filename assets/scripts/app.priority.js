d1528b613bbceb25383c1a945f3797d8868ebd33
Nathan Bolton <nathan@nathanbolton.ca>
2019-11-12 22:33:21 -0800

    changed lazy loading classes

--- assets/scripts/app.priority.js
+++ assets/scripts/app.priority.js
@@ -18,4 +18,6 @@ import '../../node_modules/fg-loadcss/src/cssrelpreload.js';
 import '../../node_modules/lazysizes/lazysizes.js';
 import '../../node_modules/lazysizes/plugins/respimg/ls.respimg.js';
 import '../../node_modules/lazysizes/plugins/bgset/ls.bgset.js';
-import '../../node_modules/lazysizes/plugins/native-loading/ls.native-loading.js';
+
+lazySizes.cfg.loadingClass = 'is-lazyloading';
+lazySizes.cfg.loadedClass = 'is-lazyloaded';
