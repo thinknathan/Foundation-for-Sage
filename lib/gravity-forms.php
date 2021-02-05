12a709f1ae040abfc7677e1b8d983b3e5df47ef9
Nathan Bolton <nathan@nathanbolton.ca>
2021-02-04 23:05:41 -0800

    upgrade jQ version

--- lib/gravity-forms.php
+++ lib/gravity-forms.php
@@ -7,6 +7,6 @@ namespace Roots\Sage\GravityForms;
  * Not sure how stable this is, seems to make ajax forms buggy
  */
 function add_jquery() {
-  wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-1.12.4.min.js', array(), null, false );
+  wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.5.1.min.js', array(), null, false );
 }
 add_action( 'gform_enqueue_scripts',  __NAMESPACE__ . '\\add_jquery', 10 );
