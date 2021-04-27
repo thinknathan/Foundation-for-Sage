bbc3ea334b0420a2df02118bb80fa9388b2a7206
Nathan Bolton <nathan@nathanbolton.ca>
2021-04-27 10:33:43 -0700

    Update markup support

--- lib/setup.php
+++ lib/setup.php
@@ -71,7 +71,7 @@ function setup() {
 
   // Enable HTML5 markup support
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
-  add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+  add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form', 'script', 'style']);
 
   // Support Yoast SEO Breadcrumbs
   // https://kb.yoast.com/kb/implement-wordpress-seo-breadcrumbs/
